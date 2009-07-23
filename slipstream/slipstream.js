4d6936365109db4190f5eef649d4a85f6dde7ea8
Jeff Balogh <me@jeffbalogh.org>
2009-07-22 22:19:41 -0700

    switching to github-hosted css file

--- slipstream/slipstream.js
+++ slipstream/slipstream.js
@@ -2,7 +2,7 @@ jetpack.future.import("pageMods");
 
 jetpack.pageMods.add(function(doc){
   jQuery('<link rel="stylesheet" type="text/css" ' +
-          'href="http://www.helvetireader.com/css/helvetireader.css">')
+          'href="http://jbalogh.github.com/jetpacks/slipstream/helvetireader.css">')
   .appendTo(doc.querySelector('head'));
 },
 ['http://*.google.com/reader*']);
