2aeccd7b8b77ae6734956b293967fa872a03aaa9
Jeff Balogh <me@jeffbalogh.org>
2009-11-12 15:13:21 -0800

    need to pass the document when using jquery to create elements

--- slipstream/slipstream.js
+++ slipstream/slipstream.js
@@ -2,7 +2,8 @@ jetpack.future.import("pageMods");
 
 jetpack.pageMods.add(function(doc){
   jQuery('<link rel="stylesheet" type="text/css" ' +
-          'href="http://jbalogh.github.com/jetpacks/slipstream/helvetireader.css">')
+         'href="http://jbalogh.github.com/jetpacks/slipstream/helvetireader.css">',
+         doc)
   .appendTo(doc.querySelector('head'));
 },
 ['http://*.google.com/reader*']);
