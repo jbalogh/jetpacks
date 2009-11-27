cc1b162e09f4016d4ed292f3981a03062d9d72fc
Jeff Balogh <me@jeffbalogh.org>
2009-11-27 00:11:27 -0800

    script for building gh-pages

---
+++ regen.sh
@@ -0,0 +1,10 @@
+# Not an executable so it keeps the same python environment.
+
+ROOT="gh-pages"
+
+./builder.py
+
+for f in $(find . -name index.html | grep -v "./$ROOT")
+    D="$ROOT/$(dirname $f)" && mkdir -p $D && cp $f $D
+
+cp -r media $ROOT
